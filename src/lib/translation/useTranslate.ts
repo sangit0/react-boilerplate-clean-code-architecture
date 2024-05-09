f22e714b7a1bfc0d2079ae47ae0b1a9a614b1613
Sangit <sangit.barua@un.org>
2024-05-09 12:56:14 +0700

    button ui element

---
+++ src/lib/translation/useTranslate.ts
@@ -0,0 +1,9 @@
+/**
+ * Hook for translating text
+ * @returns (text: string) => string
+ */
+export const useTranslate = () => {
+  return (text: string) => {
+    return text;
+  };
+};
---
+++ src/lib/ui/elements/Button/Button.tsx
@@ -0,0 +1,28 @@
+import { ButtonProps, Button as ChakraButton } from '@chakra-ui/react';
+import { useTranslate } from '~/lib/translation';
+
+type ButtonElementProps = {
+  dontTranslate?: boolean;
+} & ButtonProps;
+
+/**
+ * Button element
+ * @param dontTranslate Boolean to control translation
+ * @param children Button content
+ * @param rest Other button properties
+ * @returns JSX.Element
+ */
+export const Button = ({
+  dontTranslate,
+  children,
+  ...rest
+}: ButtonElementProps) => {
+  const shouldTranslate = !dontTranslate && typeof children === 'string';
+  const translate = useTranslate();
+
+  return (
+    <ChakraButton {...rest}>
+      {shouldTranslate ? translate(children as string) : children}
+    </ChakraButton>
+  );
+};
